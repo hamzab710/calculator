e0760bffeee00c09e46cf39689997e93f631e80b
hamzab710 <hamzabessali8@gmail.com>
2023-09-16 20:36:33 +0100

    Create functions for the main operations create a sum,subtract, multiply and divide functions create operate function that takes an operator and 2 numbers and then calls one of the above functions on the numbers.

---
+++ script.js
@@ -0,0 +1,24 @@
+function sum(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    if (b == 0) return "Can not divide by zero"
+    return a / b
+}
+
+function operate(operator, a, b) {
+    if (operator == "+") return sum(a, b);
+    else if (operator == "-") return subtract(a, b);
+    else if (operator == "*") return multiply(a, b);
+    else if (operator == "/") return divide(a, b);
+    else return "Invalid operator"
+}
