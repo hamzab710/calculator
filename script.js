018a4ea3da1270a7951b285527a8f76438dda910
hamzab710 <hamzabessali8@gmail.com>
2023-09-18 19:02:59 +0100

    Make the calculator work for 2 numbers
    The displayNumbers function displays the first number the the second
    number after clicking an operator.
    
    The calculateResult function is called when clicking the equal button,
    and is using the operate function to calculate depending on the
    operator button clicked.
    
    StoreNumber() is not ideal. deleted.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let operator = null;
+let firstNumber = null;
+let secondNumber = null;
+let waitingForSecondNumber = false;
+
 function sum(a, b) {
     return a + b
 }
@@ -15,27 +20,11 @@ function divide(a, b) {
     return a / b
 }
 
-function operate(operator, a, b) {
-    if (operator == "+") return sum(a, b);
-    else if (operator == "-") return subtract(a, b);
-    else if (operator == "*") return multiply(a, b);
-    else if (operator == "/") return divide(a, b);
-    else return "Invalid operator"
-}
-let latestInput;
-function displayNumbers (event) {
-    const buttonText = event.target.getAttribute('data-text');
-    latestInput = document.querySelector('p');
-    const displayElement = document.querySelector('.display');
-    if (latestInput) {
-        latestInput.textContent += buttonText;
-        latestInput.classList.add('.firstNumber');
-    } else {
-        let input = document.createElement('p');
-        input.textContent = buttonText;
-        input.classList.add('.firstNumber');
-        displayElement.appendChild(input);
-    }
+function operate(operator, firstNumber, secondNumber) {
+    if (operator == "+") return sum(firstNumber, secondNumber);
+    else if (operator == "-") return subtract(firstNumber, secondNumber);
+    else if (operator == "*") return multiply(firstNumber, secondNumber);
+    else if (operator == "/") return divide(firstNumber, secondNumber);
 }
 
 let displayed = document.querySelectorAll('.displayed');
@@ -44,15 +33,46 @@ displayed.forEach(function(element) {
     element.addEventListener('click', displayNumbers);
 });
 
-let storedNumber;
-function storeNumbers() {
-    storedNumber = parseInt(latestInput.textContent);
-    latestInput.parentNode.removeChild(latestInput);
+function displayNumbers (event) {
+    const buttonText = event.target.getAttribute('data-text');
+    const displayElement = document.querySelector('.display');
+    if (waitingForSecondNumber) {
+        // Second number input
+        if (secondNumber === null) {
+            secondNumber = '';
+        }
+        secondNumber += buttonText;
+    } else {
+        // First number input
+        if (firstNumber === null) {
+            firstNumber = '';
+        }
+        firstNumber += buttonText;
+    }
+    displayElement.textContent = waitingForSecondNumber ? secondNumber : firstNumber;
 }
 
-let operator = document.querySelectorAll('.operator');
+let operatorButtons = document.querySelectorAll('.operator');
 
-operator.forEach(function(element) {
-    element.addEventListener('click', storeNumbers);
-});
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        operator = button.getAttribute('data-operator');
+        waitingForSecondNumber = true;
+    })
+})
+let result;
+function calculateResult() {
+    if (operator && firstNumber !== null && waitingForSecondNumber) {
+        const result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+        const displayElement = document.querySelector('.display');
+        displayElement.textContent = result;
 
+        // Reset the state
+        firstNumber = null;
+        waitingForSecondNumber = false;
+    }
+}
+
+let equal = document.querySelector('.equal');
+
+equal.addEventListener('click', calculateResult);
