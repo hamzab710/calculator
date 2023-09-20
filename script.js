d6d9d42345ba6204b11938113bf78877dd5687ed
hamzab710 <hamzabessali8@gmail.com>
2023-09-20 15:09:33 +0100

    Make the calculator work! the calculator now handles multiple operations due to modifications in the displayNumbers and calculateResult functions. the clear button now works thanks to the clearAll function. the delete button now works thanks to the deleteNum function.

--- script.js
+++ script.js
@@ -2,29 +2,30 @@ let operator = null;
 let firstNumber = null;
 let secondNumber = null;
 let waitingForSecondNumber = false;
+let resultCalculated = false;
 
 function sum(a, b) {
-    return a + b
+    return a + b;
 }
 
 function subtract(a, b) {
-    return a - b
+    return a - b;
 }
 
 function multiply(a, b) {
-    return a * b
+    return a * b;
 }
 
 function divide(a, b) {
-    if (b == 0) return "Can not divide by zero"
-    return a / b
+    if (b === 0) return "Can not divide by zero";
+    return a / b;
 }
 
 function operate(operator, firstNumber, secondNumber) {
-    if (operator == "+") return sum(firstNumber, secondNumber);
-    else if (operator == "-") return subtract(firstNumber, secondNumber);
-    else if (operator == "*") return multiply(firstNumber, secondNumber);
-    else if (operator == "/") return divide(firstNumber, secondNumber);
+    if (operator === "+") return sum(firstNumber, secondNumber);
+    else if (operator === "-") return subtract(firstNumber, secondNumber);
+    else if (operator === "*") return multiply(firstNumber, secondNumber);
+    else if (operator === "/") return divide(firstNumber, secondNumber);
 }
 
 let displayed = document.querySelectorAll('.displayed');
@@ -33,46 +34,101 @@ displayed.forEach(function(element) {
     element.addEventListener('click', displayNumbers);
 });
 
-function displayNumbers (event) {
+function displayNumbers(event) {
     const buttonText = event.target.getAttribute('data-text');
     const displayElement = document.querySelector('.display');
+    if (resultCalculated) {
+        // If a result has been calculated, set firstNumber to the result
+        // and clear the secondNumber
+        firstNumber = result;
+        secondNumber = null;
+        resultCalculated = false;
+    }
     if (waitingForSecondNumber) {
         // Second number input
         if (secondNumber === null) {
             secondNumber = '';
         }
         secondNumber += buttonText;
+        displayElement.textContent = secondNumber;
     } else {
         // First number input
         if (firstNumber === null) {
             firstNumber = '';
         }
         firstNumber += buttonText;
+        displayElement.textContent = firstNumber;
     }
-    displayElement.textContent = waitingForSecondNumber ? secondNumber : firstNumber;
 }
 
 let operatorButtons = document.querySelectorAll('.operator');
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
-        operator = button.getAttribute('data-operator');
-        waitingForSecondNumber = true;
-    })
-})
-let result;
-function calculateResult() {
-    if (operator && firstNumber !== null && waitingForSecondNumber) {
-        const result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
-        const displayElement = document.querySelector('.display');
-        displayElement.textContent = result;
-
-        // Reset the state
-        firstNumber = null;
-        waitingForSecondNumber = false;
-    }
-}
+        if (!resultCalculated) {
+            if (waitingForSecondNumber) {
+                // If a second operator is clicked before calculating,
+                // calculate the current operation first
+                calculateResult();
+            }
+            operator = button.getAttribute('data-operator');
+            waitingForSecondNumber = true;
+        } else {
+            // If a result has been calculated, use it as the new firstNumber
+            firstNumber = result;
+            operator = button.getAttribute('data-operator');
+            waitingForSecondNumber = true;
+            resultCalculated = false;
+        }
+    });
+});
 
 let equal = document.querySelector('.equal');
 
 equal.addEventListener('click', calculateResult);
+
+function calculateResult() {
+    if (operator && firstNumber !== null && waitingForSecondNumber) {
+        result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+        const displayElement = document.querySelector('.display');
+        displayElement.textContent = result;
+        // Reset the state for further calculations
+        firstNumber = result;
+        secondNumber = null;
+        waitingForSecondNumber = false;
+        resultCalculated = true;
+    }
+}
+
+// reset the calculator's state and clear the display when the clear button is clicked.
+function clearAll() {
+    const displayElement = document.querySelector('.display');
+    displayElement.textContent = "";
+    operator = null;
+    firstNumber = null;
+    secondNumber = null;
+    waitingForSecondNumber = false;
+    resultCalculated = false;
+}
+
+let clear = document.querySelector('.clear');
+
+clear.addEventListener('click', clearAll);
+
+// delete the last number entered
+function deleteNum() {
+    const displayElement = document.querySelector('.display');
+    if (resultCalculated) {
+        clearAll()
+    } else if (waitingForSecondNumber) {
+        secondNumber = secondNumber.slice(0, -1);
+        displayElement.textContent = secondNumber;
+    } else {
+        firstNumber = firstNumber.slice(0 , -1);
+        displayElement.textContent = firstNumber;
+    }
+};
+
+let cancel = document.querySelector('.delete');
+
+cancel.addEventListener('click', deleteNum);
