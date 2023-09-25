0509e259984baae6e045c6c6d5bb53eee9d0d4ba
hamzab710 <hamzabessali8@gmail.com>
2023-09-25 21:15:34 +0100

    Add footer, improve styling and script Changed the lengths units to relative units. Add shadows for container and buttons. Reorganized the script and minimized the handleKeyPress function.

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let secondNumber = null;
 let waitingForSecondNumber = false;
 let resultCalculated = false;
 
+let displayed = document.querySelectorAll('.displayed');
+let operatorButtons = document.querySelectorAll('.operator');
+let equal = document.querySelector('.equal');
+let clear = document.querySelector('.clear');
+let cancel = document.querySelector('.delete');
+let decimal = document.querySelector('.dot');
+
 function sum(a, b) {
     return a + b;
 }
@@ -28,11 +35,6 @@ function operate(operator, firstNumber, secondNumber) {
     else if (operator === "/") return divide(firstNumber, secondNumber);
 }
 
-let displayed = document.querySelectorAll('.displayed');
-
-displayed.forEach(function(element) {
-    element.addEventListener('click', displayNumbers);
-});
 
 function displayNumbers(event) {
     const buttonText = event.target.getAttribute('data-text');
@@ -52,10 +54,8 @@ function displayNumbers(event) {
         secondNumber += buttonText;
         displayElement.textContent = secondNumber;
         if (displayElement.textContent.includes('.')) {
-            // Disable the decimal button
             decimal.disabled = true;
         } else {
-            // Enable the decimal button
             decimal.disabled = false;
         }
     } else {
@@ -68,7 +68,93 @@ function displayNumbers(event) {
     }
 }
 
-let operatorButtons = document.querySelectorAll('.operator');
+function calculateResult() {
+    if (operator && firstNumber !== null && waitingForSecondNumber) {
+        result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+        const displayElement = document.querySelector('.display');
+        displayElement.textContent = result;
+        // Reset the state for further calculations
+        firstNumber = result;
+        secondNumber = null;
+        waitingForSecondNumber = false;
+        resultCalculated = true;
+        if (displayElement.textContent.includes('.')) {
+            decimal.disabled = true;
+        } else {
+            decimal.disabled = false;
+        }
+    }
+}
+
+// reset the calculator's state and clear the display when the clear button is clicked.
+function clearAll() {
+    const displayElement = document.querySelector('.display');
+    displayElement.textContent = "";
+    operator = null;
+    firstNumber = null;
+    secondNumber = null;
+    waitingForSecondNumber = false;
+    resultCalculated = false;
+    decimal.disabled = false;
+}
+
+// delete the last number entered
+function deleteNum() {
+    const displayElement = document.querySelector('.display');
+    if (resultCalculated) {
+        clearAll()
+    } else if (waitingForSecondNumber) {
+        secondNumber = secondNumber.slice(0, -1);
+        displayElement.textContent = secondNumber;
+    } else {
+        firstNumber = firstNumber.slice(0 , -1);
+        displayElement.textContent = firstNumber;
+    }
+    if (displayElement.textContent.includes('.')) {
+        decimal.disabled = true;
+    } else {
+        decimal.disabled = false;
+    }
+};
+
+function disableDecimal() {
+    const displayElement = document.querySelector('.display');
+    if (displayElement.textContent.includes('.')) {
+        decimal.disabled = true;
+    }
+}
+
+function handleKeyPress(event) {
+    const keyPressed = event.key;
+  
+    const buttons = document.querySelectorAll('.displayed');
+    buttons.forEach((button) => {
+        const buttonText = button.getAttribute('data-text');
+    
+        if (keyPressed === buttonText) {
+            button.click();
+        }
+    });
+  
+    const otherButtons = document.querySelectorAll('.not-displayed');
+    otherButtons.forEach((button) => {
+        const buttonFunction = button.getAttribute('data-text');
+
+        if (keyPressed === buttonFunction) {
+            button.click();
+        }
+    });
+}
+
+equal.addEventListener('click', calculateResult);
+clear.addEventListener('click', clearAll);
+cancel.addEventListener('click', deleteNum);
+decimal.addEventListener('click', disableDecimal);
+document.addEventListener("keydown", handleKeyPress);
+
+displayed.forEach(function(element) {
+    element.addEventListener('click', displayNumbers);
+});
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -90,128 +176,3 @@ operatorButtons.forEach(button => {
         }
     });
 });
-
-let equal = document.querySelector('.equal');
-
-equal.addEventListener('click', calculateResult);
-
-function calculateResult() {
-    if (operator && firstNumber !== null && waitingForSecondNumber) {
-        result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
-        const displayElement = document.querySelector('.display');
-        displayElement.textContent = result;
-        // Reset the state for further calculations
-        firstNumber = result;
-        secondNumber = null;
-        waitingForSecondNumber = false;
-        resultCalculated = true;
-        if (displayElement.textContent.includes('.')) {
-            // Disable the decimal button
-            decimal.disabled = true;
-        } else {
-            decimal.disabled = false;
-        }
-    }
-}
-
-// reset the calculator's state and clear the display when the clear button is clicked.
-function clearAll() {
-    const displayElement = document.querySelector('.display');
-    displayElement.textContent = "";
-    operator = null;
-    firstNumber = null;
-    secondNumber = null;
-    waitingForSecondNumber = false;
-    resultCalculated = false;
-    decimal.disabled = false;
-}
-
-let clear = document.querySelector('.clear');
-
-clear.addEventListener('click', clearAll);
-
-// delete the last number entered
-function deleteNum() {
-    const displayElement = document.querySelector('.display');
-    if (resultCalculated) {
-        clearAll()
-    } else if (waitingForSecondNumber) {
-        secondNumber = secondNumber.slice(0, -1);
-        displayElement.textContent = secondNumber;
-    } else {
-        firstNumber = firstNumber.slice(0 , -1);
-        displayElement.textContent = firstNumber;
-    }
-    if (displayElement.textContent.includes('.')) {
-        // Disable the decimal button
-        decimal.disabled = true;
-    } else {
-        // Enable the decimal button
-        decimal.disabled = false;
-    }
-};
-
-let cancel = document.querySelector('.delete');
-
-cancel.addEventListener('click', deleteNum);
-
-let decimal = document.querySelector('.dot');
-
-decimal.addEventListener('click', disableDecimal);
-
-function disableDecimal() {
-    // Check if you should disable the decimal button
-    const displayElement = document.querySelector('.display');
-    if (displayElement.textContent.includes('.')) {
-        // Disable the decimal button
-        decimal.disabled = true;
-    }
-}
-
-// Function to trigger the button click event
-function handleKeyPress(event) {
-    const keyPressed = event.key;
-  
-    const buttons = document.querySelectorAll('.displayed');
-    buttons.forEach((button) => {
-        const buttonText = button.getAttribute('data-text');
-      
-        // Check if the pressed key matches the button's data-text
-        if (keyPressed === buttonText) {
-            button.click();
-        }
-    });
-  
-    const operatorButtons = document.querySelectorAll('.operator');
-    operatorButtons.forEach((button) => {
-        const operatorText = button.getAttribute('data-text');
-      
-        // Check if the pressed key matches the operator button's data-text
-        if (keyPressed === operatorText) {
-            button.click();
-        }
-    });
-
-    const equal = document.querySelector('.equal');
-    const equalText = equal.getAttribute('data-text');
-
-    if (keyPressed === equalText) {
-        equal.click();
-    }
-
-    const clear = document.querySelector('.clear');
-    const clearText = clear.getAttribute('data-text');
-
-    if (keyPressed === clearText) {
-        clear.click();
-    }
-
-    const cancel = document.querySelector('.delete');
-    const cancelText = cancel.getAttribute('data-text');
-
-    if (keyPressed === cancelText) {
-        cancel.click();
-    }
-}
-  
-document.addEventListener("keydown", handleKeyPress);
